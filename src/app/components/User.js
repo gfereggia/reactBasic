fd9534be5ca12b79fddb9a3b9acda6049f3b736f
gfereggia <german@dya.com.ar>
2018-10-11 12:58:16 -0300

    final spa

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { browserHistory } from "react-router";
 
 export class User extends React.Component {
-	render() {
-		return (
-			<div>
-				<h3></h3>
-				<p>User ID:</p>
-			</div>
-		);
-	}
+    onNavigateHome() {
+        browserHistory.push("/home");
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>The User Page</h3>
+                <p>User ID: {this.props.params.id}</p>
+                <button onClick={this.onNavigateHome} className="btn btn-primary">Go Home!</button>
+            </div>
+        );
+    }
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,25 @@
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
+import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
 //Components
-import { Root } from "./components/Root";
-import { Home } from "./components/Home";
+import {Root} from "./components/Root";
+import {Home} from "./components/Home";
+import {User} from "./components/User";
 
 class App extends React.Component {
-
-	render() {
-		return (
-			<Root>
-				<Home></Home>
-			</Root>
-		);
-	}
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path={"/"} component={Root} >
+                    <IndexRoute component={Home} />
+                    <Route path={"user/:id"} component={User} />
+                    <Route path={"home"} component={Home} />
+                </Route>
+                <Route path={"home-single"} component={Home}/>
+            </Router>
+        );
+    }
 }
 
-render(<App/>, window.document.getElementById("app"));
+render(<App />, window.document.getElementById('app'));
